972883fbbe443e1ce530ee2074a4457a0820d867
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-04-11 00:04:43 +0600

    date passed and showed realtime

--- src/java/com/httppackage/httpServlet.java
+++ src/java/com/httppackage/httpServlet.java
@@ -15,13 +15,14 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 /**
  *
  * @author Md Sajjad Hossin
  */
 public class httpServlet extends HttpServlet {
-        public void doGet(HttpServlet request, HttpServletResponse response)throws ServletException,IOException{
+        public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException{
         System.out.println("This is get method..........");
         
         response.setContentType("text/html");
@@ -29,5 +30,6 @@ public class httpServlet extends HttpServlet {
         PrintWriter out=response.getWriter();
         
         out.print("<h1>This is get method of my servlet</h1>");
+        out.println(new Date().toString());
     }
 }
